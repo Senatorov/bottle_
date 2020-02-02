025d93f87c228b448254b79dd4fef6acd216afe2
user <senatorov.nikolay@mail.ru>
2020-02-02 09:22:00 +0300

    '0.6'

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from bottle import *
 import sqlite3
 
-@route('/greeting')
-def greeting(username):
-    return template('frame/greet', username=username)
+@route('/')
+def authorization_fail():
+    return template('frame/authorization_fail')
+
 
 @route('/')
 def authorization():
@@ -13,11 +14,53 @@ def authorization():
 def do_login():
     username = request.forms.get('username')
     password = request.forms.get('password')
+    sign = request.forms.get('Sign')
+    registr = request.forms.get('Registr')
 
-    if (username == '1') and (password == '1'):
-        return greeting(username)
+    if sign:
+        if (username == '1') and (password == '1'):
+            return greeting(username)
+        else:
+            return authorization_fail()
+
+    if registr:
+        return regisration()
+
+
+
+@route('/greeting')
+def greeting(username):
+    return template('frame/greet', username=username)
+
+@route('registration')
+def regisration():
+    return template('frame/registr')
+
+@route('registration')
+def f_registration_username():
+    return template('frame/registr_fail_name')
+
+@route('registration')
+def f_registration_password():
+    return template('frame/registr_fail_password')
+
+@route('/registration', method='POST')
+def register():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    r_password = request.forms.get('r_password')
+
+    if username == '!!!': ######################################################### SQLite3 name == username!!!!######################################################
+        return f_registration_username()
+    elif password != r_password:
+        return f_registration_password()
     else:
         return authorization()
+
+
+
+
+
 
 
 run(host='localhost', port='8080', debug=True)
